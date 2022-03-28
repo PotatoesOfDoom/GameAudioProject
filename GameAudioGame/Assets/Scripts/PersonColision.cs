f1518fd67fcfd0b2ce9d3376428373b2e56e50c3
PotatoesOfDoom <46922832+PotatoesOfDoom@users.noreply.github.com>
2022-03-28 16:58:36 +0100

    Car interaction

--- GameAudioGame/Assets/Scripts/PersonColision.cs
+++ GameAudioGame/Assets/Scripts/PersonColision.cs
@@ -14,5 +14,13 @@ public class PersonColision : MonoBehaviour
             //person.GetComponent<Rigidbody>().AddExplosionForce(5, person.transform.position, 1.0f, 3.0F);
             person.GetComponent<Collider>().enabled = false;
         }
+
+        if (collision.gameObject.tag == "Car")
+        {
+            Debug.Log("explode");
+            var car = collision.gameObject;
+            car.GetComponent<Rigidbody>().AddExplosionForce(500, car.transform.position, 10.0f, 10.0F);
+            //person.GetComponent<Collider>().enabled = false;
+        }
     }
 }
